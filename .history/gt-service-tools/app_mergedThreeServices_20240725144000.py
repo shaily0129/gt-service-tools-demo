ca496070e59fe9a07d848bb2234b8328c48e0b3a
shaily0129 <shaily@simwerx.com>
2024-07-25 15:00:29 -0600

    added 4 web-enabled services

---
+++ .history/gt-service-tools/app_mergedThreeServices_20240725144000.py
@@ -0,0 +1,251 @@
+import json
+from typing import Dict, List
+import uvicorn
+from fastapi import FastAPI, HTTPException, Request, Body
+from pydantic import BaseModel
+from caching.CacheRedis import RedisManager
+from services.models.Models import (
+    FinalAssetInteractionRequest,
+    TriageInteractionRequest,
+    TriageInteractionRequest1,
+    PatientMatrixInteractionRequest
+)
+from services.service_triage_category.algos.pyreason.algo_triage_basic.AlgoFinalAssetInteraction import (
+    FinalAssetInteraction,
+)
+from services.service_triage_category.algos.pyreason.algo_triage_basic.AlgoTriageScoreInteraction import (
+    TriageScoreInteraction,
+)
+from services.service_triage_category.algos.pyreason.algo_triage_basic.AlgoTriageCategoryInteraction import (
+    TriageCategoryBasic,
+)
+from services.service_patient_priority_matrix.algos.pyreason.algo_patient_matrix_basic.AlgoPatientMatrixInteraction import (
+    PatientMatrixInteraction,
+)
+from services.service_triage.factory.FactoryAlgoTriage import (
+    Threshold,
+)
+from utils.Utils import load_env_file
+import logging
+
+# Setup logging
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+app = FastAPI(
+    title="ASU Tools",
+    description="Demo of using interactive tools",
+    version="0.0.1",
+)
+
+class PatientParams(BaseModel):
+    patient_id: str  # Mandatory field
+    params: dict
+
+class TriageRequestBody(BaseModel):
+    request_id: str
+    patients: List[PatientParams]
+
+# Define thresholds_data_algo3
+thresholds_data_algo3 = {
+    "external_hemorrhage": Threshold(min_value=1, max_value=6),
+    "tension_pneumothorax": Threshold(min_value=1, max_value=6),
+    "traumatic_brain_injury": Threshold(min_value=1, max_value=6),
+    "concussion": Threshold(min_value=1, max_value=6),
+    "cerebral_contusion": Threshold(min_value=1, max_value=6),
+    "subarachnoid_hemorrhage": Threshold(min_value=1, max_value=6),
+    "epidural_hematoma": Threshold(min_value=1, max_value=6),
+    "nasal_fracture": Threshold(min_value=1, max_value=6),
+    "orbital_fracture": Threshold(min_value=1, max_value=6),
+    "le_fort_II_fracture": Threshold(min_value=1, max_value=6),
+    "rib_fracture": Threshold(min_value=1, max_value=6),
+    "lung_contusion": Threshold(min_value=1, max_value=6),
+    "flail_chest": Threshold(min_value=1, max_value=6),
+    "aortic_laceration": Threshold(min_value=1, max_value=6),
+    "minor_liver_laceration": Threshold(min_value=1, max_value=6),
+    "splenic_laceration": Threshold(min_value=1, max_value=6),
+    "liver_hematoma": Threshold(min_value=1, max_value=6),
+    "pancreatic_transection": Threshold(min_value=1, max_value=6),
+    "radius_ulna_fracture": Threshold(min_value=1, max_value=6),
+    "femur_fracture": Threshold(min_value=1, max_value=6),
+    "knee_dislocation": Threshold(min_value=1, max_value=6),
+    "traumatic_amputation_below_knee": Threshold(min_value=1, max_value=6),
+    "traumatic_amputation_above_knee": Threshold(min_value=1, max_value=6),
+    "burn": Threshold(min_value=1, max_value=6),
+    "gcs": Threshold(min_value=3, max_value=15),
+    "sbp": Threshold(min_value=0, max_value=219),
+    "rr": Threshold(min_value=0, max_value=100),
+}
+
+@app.post("/tools/final_asset", tags=["asset"])
+async def rate_response(
+    request: Request, asset: FinalAssetInteractionRequest
+) -> FinalAssetInteractionRequest:
+    try:
+        # Step 1. Setup Caching Manager
+        load_env_file("dev.env")
+        caching_manager = RedisManager()
+        key = f"tools-asset-{asset.request_id}"
+
+        # Clear cache before processing the request
+        caching_manager.delete(key)
+
+        # Step 2. Check for new or complete interaction request
+        cached_asset_json = caching_manager.get_json(key)
+        if cached_asset_json is None:
+            caching_manager.save_json(key, asset.json())
+        else:
+            cached_asset = FinalAssetInteractionRequest(**cached_asset_json)
+            if cached_asset.complete:
+                return cached_asset
+
+        # Step 3. Interaction request
+        final_asset_interaction = FinalAssetInteraction()
+        asset = final_asset_interaction.run_final_asset_algo(
+            asset_interaction_request=asset
+        )
+        caching_manager.save_json(key, asset.json())
+
+        return asset
+
+    except Exception as e:
+        print(e)
+        raise HTTPException(status_code=500, detail=str(e))
+
+@app.post("/tools/triage", tags=["Triage"])
+async def rate_response(request: Request, body: TriageRequestBody = Body(...)) -> dict:
+    try:
+        load_env_file("dev.env")
+        caching_manager = RedisManager()
+
+        results = []
+        for patient in body.patients:
+            key = f"tools-triage-{body.request_id}-{patient.patient_id}"
+            triage_interaction_request = TriageInteractionRequest(
+                request_id=body.request_id,
+                patient_id=patient.patient_id,
+                params=patient.params,
+            )
+
+            cached_bir_json = caching_manager.get_json(key)
+            if cached_bir_json is None:
+                caching_manager.save_json(key, triage_interaction_request.json())
+            else:
+                cached_bir = TriageInteractionRequest(**cached_bir_json)
+                if cached_bir.complete:
+                    results.append(cached_bir)
+                    continue
+
+            bir = TriageScoreInteraction(
+                thresholds=thresholds_data_algo3
+            ).run_triage_algo(triage_interaction_request=triage_interaction_request)
+
+            caching_manager.save_json(key, bir.json())
+            results.append(bir)
+
+        sorted_results = sorted(
+            results, key=lambda x: x.triage_score.score, reverse=True
+        )
+
+        return {"results": sorted_results}
+
+    except Exception as e:
+        print(e)
+        raise HTTPException(status_code=500, detail=str(e))
+
+@app.post("/tools/triage/scores", tags=["Triage"])
+async def get_patient_scores(
+    request: Request, patient_ids: List[str] = Body(...)
+) -> dict:
+    try:
+        load_env_file("dev.env")
+        caching_manager = RedisManager()
+
+        results = []
+        for patient_id in patient_ids:
+            keys = caching_manager.get_keys(f"tools-triage-*-{patient_id}")
+            if not keys:
+                continue
+
+            key = keys[-1]
+            cached_patient_json = caching_manager.get_json(key)
+
+            cached_patient_dict = json.loads(cached_patient_json)
+            cached_patient = TriageInteractionRequest(**cached_patient_dict)
+            patient_record = {
+                "patient_id": cached_patient.patient_id,
+                "triage_score": cached_patient.triage_score,
+                "algo_name": cached_patient.triage_score.algo_name,
+            }
+            results.append(patient_record)
+
+        sorted_results = sorted(
+            results, key=lambda x: x["triage_score"].score, reverse=True
+        )
+
+        return {"patients": sorted_results}
+
+    except Exception as e:
+        print(e)
+        raise HTTPException(status_code=500, detail:str(e))
+
+@app.post("/tools/triage_category", tags=["Triage"])
+async def rate_response(
+    request: Request, triage: TriageInteractionRequest1
+) -> TriageInteractionRequest1:
+    try:
+        load_env_file("dev.env")
+        caching_manager = RedisManager()
+        key = f"tools-triage-{triage.request_id}"
+
+        cached_bir_json = caching_manager.get_json(key)
+        if cached_bir_json is None:
+            caching_manager.save_json(key, triage.json())
+        else:
+            cached_bir = TriageInteractionRequest1(**cached_bir_json)
+            if cached_bir.complete:
+                return cached_bir
+
+        bir = TriageCategoryBasic().run_triage_algo(triage_interaction_request=triage)
+        caching_manager.save_json(key, bir.json())
+
+        return bir
+
+    except Exception as e:
+        print(e)
+        raise HTTPException(status_code=500, detail=str(e))
+
+@app.post("/tools/patient_matrix", tags=["PatientMatrix"])
+async def patient_matrix_response(
+    request: Request, interaction_request: PatientMatrixInteractionRequest
+) -> PatientMatrixInteractionRequest:
+    try:
+        # Step 1. Setup Caching Manager
+        load_env_file("dev.env")
+        caching_manager = RedisManager()
+        key = f"tools-patient-matrix-{interaction_request.request_id or interaction_request.patient_id}"
+
+        # Step 2. Check for new or complete interaction request
+        cached_request_json = caching_manager.get_json(key)
+        if cached_request_json is None:
+            caching_manager.save_json(key, interaction_request.json())
+        else:
+            cached_request = PatientMatrixInteractionRequest(**cached_request_json)
+            if cached_request.complete:
+                return cached_request
+
+        # Step 3. Interaction request
+        print(f"Received request: {interaction_request}")  # Debug statement
+        interaction_response = PatientMatrixInteraction().run_patient_matrix_algo(
+            interaction_request=interaction_request
+        )
+        caching_manager.save_json(key, interaction_response.json())
+
+        return interaction_response
+
+    except Exception as e:
+        print(f"Error: {e}")
+        raise HTTPException(status_code=500, detail=str(e))
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8002)
---
+++ .history/gt-service-tools/services/service_patient_priority_matrix/algos/pyreason/algo_patient_matrix_basic/AlgoPatientMatrixInteraction_20240725134602.py
@@ -0,0 +1,205 @@
+from services.service_patient_priority_matrix.AlgoPatientPriorityMatrix import (
+    PatientPriority,
+)
+from services.models.ModelPatientMatrix import RationaleRecord, PatientMatrix
+from services.models.ModelPatient import Patient
+from datetime import datetime
+import networkx as nx
+import pyreason as pr
+import os
+from services.models.Models import Interaction, PatientMatrixInteractionRequest
+
+
+class PatientMatrixInteraction(PatientPriority):
+
+    def __init__(self):
+        self.interpretation = None
+        self.next_time = 0
+
+    def create_pyreason_graph(self, patients_cat: list[Patient]):
+        g = nx.DiGraph()
+        g.add_node(
+            "expectant", triage_category_id=2, type_triage_category_expectant="1,1"
+        )
+        g.add_node(
+            "immediate", triage_category_id=3, type_triage_category_immediate="1,1"
+        )
+        g.add_node("urgent", triage_category_id=4, type_triage_category_urgent="1,1")
+        g.add_node("delayed", triage_category_id=5, type_triage_category_delayed="1,1")
+        g.add_node("minor", triage_category_id=6, type_triage_category_minor="1,1")
+        g.add_node(
+            "uninjured", triage_category_id=7, type_triage_category_uninjured="1,1"
+        )
+
+        g.add_node(str(0), priority_zero="1,1")
+        g.add_node(str(1), priority_one="1,1")
+        g.add_node(str(2), priority_two="1,1")
+        g.add_node(str(3), priority_three="1,1")
+        g.add_node(str(4), priority_four="1,1")
+        g.add_node(str(5), priority_five="1,1")
+        g.add_node(str(6), priority_six="1,1")
+
+        for index, patient in enumerate(patients_cat):
+            record = patient.physiology_record
+            patient_name = record["name"]
+            patient_category = record["category"]
+            g.add_node(
+                patient_name,
+                casualty_id=index + 2,
+                type_casualty="1,1",
+                litter="0,0",
+                ambulatory="1,1",
+                medevac_needed="0,0",
+                evac_needed="0,0",
+                resupply_needed="0,0",
+            )
+            g.add_edge(patient_name, patient_category, triage_category="1,1")
+            for i in range(0, 7):
+                g.add_edge(
+                    patient_name,
+                    str(i),
+                    medevac_priority="0,0",
+                    evac_priority="0,0",
+                    resupply_priority="0,0",
+                )
+
+        return g
+
+    def write_graphml(self, nx_graph, graphml_path: str):
+        nx.write_graphml_lxml(nx_graph, graphml_path, named_key_ids=True)
+
+    def return_patient_priority_matrix(
+        self, patients: list[Patient]
+    ) -> list[PatientMatrix]:
+        patients_priority_matrices = []
+        graph = self.create_pyreason_graph(patients)
+        graphml_path = "pyreason_input_graph_triage_categories.graphml"
+        current_script_directory = os.path.dirname(os.path.abspath(__file__))
+        graphml_path = os.path.join(current_script_directory, graphml_path)
+
+        rules_path = "rules_triage_category_to_patient_priority_matrix.txt"
+        self.write_graphml(nx_graph=graph, graphml_path=graphml_path)
+        rules_path = os.path.join(current_script_directory, rules_path)
+
+        pr.reset()
+        pr.reset_rules()
+        pr.settings.verbose = False
+        pr.settings.atom_trace = True
+        pr.settings.canonical = True
+        pr.settings.inconsistency_check = False
+        pr.settings.static_graph_facts = False
+        pr.settings.save_graph_attributes_to_trace = True
+        pr.settings.store_interpretation_changes = True
+
+        pr.load_graphml(graphml_path)
+        pr.add_rules_from_file(rules_path)
+
+        self.interpretation = pr.reason(0, again=False)
+        self.next_time = self.interpretation.time + 1
+        folder_name = "traces_t0_triage_category_to_patient_priority_matrix"
+        folder_name = os.path.join(current_script_directory, folder_name)
+        if not os.path.exists(folder_name):
+            os.makedirs(folder_name)
+        pr.save_rule_trace(self.interpretation, folder_name)
+
+        patient_detail = {}
+        fields = [
+            "litter",
+            "ambulatory",
+            "medevac_needed",
+            "evac_needed",
+            "resupply_needed",
+        ]
+        for field in fields:
+            df_outer = pr.filter_and_sort_nodes(self.interpretation, [(field)])
+            for t, df in enumerate(df_outer):
+                if not df[field].empty:
+                    for i in range(len(df["component"])):
+                        patient_detail[df["component"][i]] = patient_detail.get(
+                            df["component"][i], {}
+                        )
+                        patient_detail[df["component"][i]][field] = df[field][i] == [
+                            1,
+                            1,
+                        ]
+
+        edge_fields = ["medevac_priority", "evac_priority", "resupply_priority"]
+        for field in edge_fields:
+            df_outer = pr.filter_and_sort_edges(self.interpretation, [(field)])
+            for t, df in enumerate(df_outer):
+                if not df[field].empty:
+                    for i in range(len(df["component"])):
+                        if df[field][i] == [1, 1]:
+                            patient_detail[df["component"][i][0]] = patient_detail.get(
+                                df["component"][i][0], {}
+                            )
+                            patient_detail[df["component"][i][0]][field] = df[
+                                "component"
+                            ][i][1]
+
+        for p_name, p_details in patient_detail.items():
+            patients_priority_matrices.append(
+                PatientMatrix(
+                    patient_name=p_name,
+                    datetime_seconds=int(datetime.now().timestamp()),
+                    algo_name="pyreason_basic",
+                    litter=p_details.get("litter", False),
+                    ambulatory=p_details.get("ambulatory", False),
+                    medevac_needed=p_details.get("medevac_needed", False),
+                    medevac_priority=p_details.get("medevac_priority", 0),
+                    evac_needed=p_details.get("evac_needed", False),
+                    evac_priority=p_details.get("evac_priority", 0),
+                    resupply_needed=p_details.get("resupply_needed", False),
+                    resupply_priority=p_details.get("resupply_priority", 0),
+                    confidence=1.0,
+                    rationale=None,
+                    interaction=None,
+                )
+            )
+        return patients_priority_matrices
+
+    def run_patient_matrix_algo(
+        self, interaction_request: PatientMatrixInteractionRequest
+    ) -> PatientMatrixInteractionRequest:
+        params = interaction_request.params
+
+        interactions = self.missing_values(params)
+        if interactions:
+            interaction_request.interactions = interactions
+            return interaction_request
+
+        try:
+            patient = Patient(name=params.get("name"), category=params.get("category"))
+            print(f"Created patient: {patient.physiology_record}")  # Debug statement
+            patient_matrix = self.return_patient_priority_matrix([patient])[0]
+            interaction_request.patient_matrix = patient_matrix
+            interaction_request.complete = True
+        except Exception as e:
+            print(f"Error processing patient matrix: {e}")
+            raise
+
+        return interaction_request
+
+    def missing_values(self, params: dict) -> list[Interaction]:
+        interactions = []
+        if "name" not in params or not params["name"].strip():
+            interaction_name = Interaction(
+                variable_name="name",
+                variable_type="str",
+                question="What is your name?",
+                options=None,
+                answer=None,
+                complete=False,
+            )
+            interactions.append(interaction_name)
+        if "category" not in params or not params["category"].strip():
+            interaction_category = Interaction(
+                variable_name="category",
+                variable_type="str",
+                question=f'What is the category of {params.get("name")}? Options: immediate, expectant, delayed, minor, urgent, uninjured',
+                options=None,
+                answer=None,
+                complete=False,
+            )
+            interactions.append(interaction_category)
+        return interactions
